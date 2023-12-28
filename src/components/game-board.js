718f9b03ee520eab1c90b36e4ccb5c1d0f626326
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-28 19:59:21 +0100

    Add placeShip

--- src/components/game-board.js
+++ src/components/game-board.js
@@ -1,8 +1,9 @@
 class GameBoard {
     static CellStatus = {
-        "empty": 0,
+        "water": 0,
         "water-hit": 1,
-        "ship-hit": 2,
+        "ship": 2,
+        "ship-hit": 3,
     };
 
     constructor(size) {
@@ -12,9 +13,21 @@ class GameBoard {
         for (let row = 0; row < size; row++) {
             const row = [];
             for (let col = 0; col < size; col++) {
-                row.push(GameBoard.CellStatus["empty"]);
+                row.push(GameBoard.CellStatus.water);
             }
             this.grid.push(row);
+        }
+    }
+
+    // position/direction format: [x, y]
+    // position is the edge of ship
+    placeShip(ship, position, direction) {
+        this.ships.push(ship);
+        for (let i = 0; i < ship.length; i++) {
+            const pos_x = position[0] + i * direction[0];
+            const pos_y = position[1] + i * direction[1];
+            // pos_y and pos_x are swapped row and cols are confusing...
+            this.grid[pos_y][pos_x] = GameBoard.CellStatus.ship;
         }
     }
 }
--- src/components/game-board.spec.js
+++ src/components/game-board.spec.js
@@ -1,4 +1,5 @@
 import GameBoard from './game-board';
+import Ship from './ship';
 
 describe('GameBoard', () => {
     test('size 10', () => {
@@ -8,5 +9,27 @@ describe('GameBoard', () => {
         expect(gb.size).toStrictEqual(size);
         expect(gb.grid.length).toStrictEqual(size);
         expect(gb.grid[0].length).toStrictEqual(size);
+    });
+    test('placeShip', () => {
+        const size = 10;
+        const gb = new GameBoard(size);
+        const s = new Ship(3);
+        gb.placeShip(s, [3, 3], [1, 0]);
+
+        const new_grid = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 2, 2, 2, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+
+        //If it should pass with deep equality, replace "toBe" with "toStrictEqual"
+        expect(gb.grid).toStrictEqual(new_grid);
     });
 });
