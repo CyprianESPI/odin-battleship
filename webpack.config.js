1ec7e4901db4f4d6b8f44d47565e0ce59df53a8c
CyprianESPI <cyprianespinoza@gmail.com>
2024-01-09 20:58:56 +0100

    Fix svg loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,12 @@ module.exports = {
           'css-loader'
         ]
       },
+      // For webpack v5
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        // More information here https://webpack.js.org/guides/asset-modules/
+        type: "asset",
+      },
       /*
       {
         test: /\.html$/i,
@@ -28,9 +34,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    }),
-    new HtmlWebpackInlineSVGPlugin({
-      inlineAll: true,
-    }),
+    })
   ]
 };
